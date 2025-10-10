9e4a3278b7de282fbd9d45bc37140306cb100d83
David Wünsch <dp.wuensch@gmail.com>
2025-10-10 16:41:30 +0200

    [mod] Explicit splitup into standard and robust accuracy

--- adversarial_training_box/pipeline/standard_training_module.py
+++ adversarial_training_box/pipeline/standard_training_module.py
@@ -18,11 +18,17 @@ class StandardTrainingModule(TrainingModule):
         if not experiment_tracker is None:
             experiment_tracker.watch(network, self.criterion, log_option="all", log_frequency=10)
 
+        correct_predictions_adversarial = 0
+        correct_predictions = 0
+        total_samples = 0
+
         for batch_idx, (data, target) in enumerate(data_loader):
             
             data, target = data.to(device), target.to(device)
+            conventional_data = None
 
             if not self.attack is None:
+                conventional_data = data.clone()
                 data = self.attack.compute_perturbed_image(network=network, data=data, labels=target, epsilon=self.epsilon)
                 data.to(device)
 
@@ -35,8 +41,26 @@ class StandardTrainingModule(TrainingModule):
             if not experiment_tracker is None:
                 experiment_tracker.log({"loss": loss.item()})
 
-        train_accuracy = (output.max(1)[1] == target).sum().item() / target.size(0)
-        return train_accuracy
+            if not self.attack is None:
+                predictions = output.max(1)[1]
+                correct_predictions_adversarial += (predictions == target).sum().item()
+                network.eval()
+                with torch.no_grad():
+                    conventional_output = network(conventional_data)
+                network.train()
+                predictions = conventional_output.max(1)[1]
+                correct_predictions += (predictions == target).sum().item()
+            else: 
+                predictions = output.max(1)[1]
+                correct_predictions += (predictions == target).sum().item()
+
+
+            total_samples += target.size(0)
+
+        train_accuracy = correct_predictions/total_samples
+        robust_accuracy = correct_predictions_adversarial / total_samples
+        
+        return train_accuracy, robust_accuracy
 
     def __str__(self) -> str:
         return f"standard_module_{self.criterion}_{self.attack}_{self.epsilon}"
