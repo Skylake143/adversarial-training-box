9b7a94ac0738b875281e5e2f83c74b27ee837737
David Wünsch <dp.wuensch@gmail.com>
2026-02-03 16:30:16 +0100

    [mod] Changed to original MNIST state

--- adversarial_training_box/adversarial_attack/pgd_attack.py
+++ adversarial_training_box/adversarial_attack/pgd_attack.py
@@ -17,8 +17,7 @@ class PGDAttack(AdversarialAttack):
         
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-        # bounds = (float(data.min()), float(data.max()))
-        fmodel = PyTorchModel(network, bounds=(-2.5,2.5), device=device)
+        fmodel = PyTorchModel(network, bounds=(0,1), device=device)
         attack = LinfPGD(abs_stepsize=self.epsilon_step_size, steps=self.number_iterations, random_start=self.random_init)
 
         raw_advs, adversaries, success = attack(fmodel, data, labels, epsilons=epsilon)
